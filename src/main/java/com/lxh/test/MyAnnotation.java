af1b46ac47d7df387523c9b7e48b8ab3a4a77d7b
liuxh613 <liuxh613@163.com>
2017-08-29 14:31:32 +0800

    增加功能

--- src/main/java/com/lxh/test/MyAnnotation.java
+++ src/main/java/com/lxh/test/MyAnnotation.java
@@ -6,5 +6,8 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface MyAnnotation {
-
+    public String name() default "fieldName";
+    public String setFuncName() default "setField";
+    public String getFuncName() default "getField";
+    public boolean defaultDBValue() default false;
 }
